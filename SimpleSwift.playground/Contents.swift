2d9d807d6d42515a26243864c3e86387e4bf3acf
Lauren Ng <2014laurenng@gmail.com>
2021-04-07 22:03:00 -0700

    completed assignment

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,17 +1,67 @@
 print("Welcome to the UW Calculator Playground")
 
+
+// array format
 func calculate(_ args: [String]) -> Int {
-    return -1
+    if(args.count == 1) {
+        return 0
+    } else if (args[1] == "-" || args[1] == "+" || args[1] == "%" || args[1] == "/" || args[1] == "*") {
+        return simpleMath(lhs: args[0], rhs: args[2], symbol: args[1])
+    } else if (args.contains("count")) {
+        let arrayLength = args.count
+        return arrayLength - 1
+    } else if (args.contains("avg")) {
+        var sum = 0;
+        for value in args {
+            if (value != "avg") {
+                let num = Int(value)
+                sum += num!
+            }
+        }
+        let count = args.count - 1
+        return sum / count
+    } else if (args.contains("fact")) {
+        let times = Int(args[0])!
+        if (times == 0) {
+            return 1
+        }
+        var total = 1
+        for i in 1...times {
+            total = total * i
+        }
+        return total
+    }
+    return 0
 }
 
+func simpleMath(lhs: String, rhs: String, symbol: String) -> Int{
+    let left = Int(lhs)
+    let right = Int(rhs)
+    
+    if (symbol == "+") {
+        return left! + right!
+    } else if (symbol == "-") {
+        return left! - right!
+    } else if (symbol == "%") {
+        return left! % right!
+    } else if (symbol == "/") {
+        return left! / right!
+    } else {
+        return left! * right!
+    }
+}
+
+// single string
 func calculate(_ arg: String) -> Int {
-    return -1
+    let array: [String] = arg.split(separator: " ").map({ substr in String(substr) })
+    let final = calculate(array)
+    return final
 }
 
 // -------------------------------------------
 // All of these expressions should return true
 // if you have implemented calculate() correctly
-//
+
 calculate(["2", "+", "2"]) == 4
 calculate(["4", "+", "4"]) == 8
 calculate(["2", "-", "2"]) == 0
@@ -53,7 +103,7 @@ calculate("5 fact") == 120
 
 // Implement calculate([String]) and calculate(String)
 // to handle negative numbers
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -68,23 +118,50 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
- 
+
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
-func calculate(_ args: [String]) -> Double {
-    return -1.0
-}
-func calculate(_ arg: String) -> Double {
+
+func calculateDouble(_ args: [String]) -> Double {
+     if(args.count == 1) {
+        return 0.0
+     } else if (args[1] == "-" || args[1] == "+" || args[1] == "%" || args[1] == "/" || args[1] == "*") {
+        return simpleDoubleMath(lhs: args[0], rhs: args[2], symbol: args[1])
+     } else if (args.contains("count")) {
+        let arrayLength = Double(args.count)
+        return arrayLength - 1.0
+     }
     return -1.0
 }
 
-calculate(["2.0", "+", "2.0"]) == 4.0
-calculate([".5", "+", "1.5"]) == 2.0
-calculate(["12.0", "-", "12.0"]) == 0.0
-calculate(["2.5", "*", "2.5"]) == 6.25
-calculate(["2.0", "/", "2.0"]) == 1.0
-calculate(["2.0", "%", "2.0"]) == 0.0
-calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
-*/
+func calculateDouble(_ arg: String) -> Double {
+    let array: [String] = arg.split(separator: " ").map({ substr in String(substr) })
+    let final = calculateDouble(array)
+    return final
+}
+
+func simpleDoubleMath(lhs: String, rhs: String, symbol: String) -> Double{
+    let left = Double(lhs)!
+    let right = Double(rhs)!
+    
+    if (symbol == "+") {
+        return left + right
+    } else if (symbol == "-") {
+        return left - right
+    } else if (symbol == "%") {
+        return left.truncatingRemainder(dividingBy: right)
+    } else if (symbol == "/") {
+        return left / right
+    } else {
+        return left * right
+    }
+}
+
+calculateDouble(["2.0", "+", "2.0"]) == 4.0
+calculateDouble([".5", "+", "1.5"]) == 2.0
+calculateDouble(["12.0", "-", "12.0"]) == 0.0
+calculateDouble(["2.5", "*", "2.5"]) == 6.25
+calculateDouble(["2.0", "/", "2.0"]) == 1.0
+calculateDouble(["2.0", "%", "2.0"]) == 0.0
+calculateDouble("1.0 2.0 3.0 4.0 5.0 count") == 5
+
